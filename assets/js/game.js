83498fb21764ea62908facaee4955e73161348e7
szlend <simon@zlender.si>
2020-07-02 13:47:21 +0200

    Frontend cleanup

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,46 +19,36 @@ export default {
 
       this.x = BigInt(0)
       this.y = BigInt(0)
-      this.lastX = BigInt(0)
-      this.lastY = BigInt(0)
-      this.moving = false
-      this.moved = false
-      this.touchX = 0
-      this.touchY = 0
+      this.width = 0
+      this.height = 0
       this.actions = []
       this.containerPool = []
 
-      this.canvas.addEventListener("mousedown", () => this.moving = true)
-      this.canvas.addEventListener("mouseup", () => this.moving = this.moved = false)
-      this.canvas.addEventListener("mouseout", () => this.moving = this.moved = false)
+      this.input = {
+        updateX: BigInt(0),
+        updateY: BigInt(0),
+        clientX: 0,
+        clientY: 0,
+        startClientX: 0,
+        startClientY: 0,
+        moving: false,
+      }
 
-      this.canvas.addEventListener("mousemove", (e) => {
-        if (this.moving) {
-          this.moved = true
-          this.x -= BigInt(Math.round(e.movementX))
-          this.y -= BigInt(Math.round(e.movementY))
-        }
-      })
-
-      this.canvas.addEventListener("touchstart", (e) => {
-        this.touchX = e.touches[0].clientX
-        this.touchY = e.touches[0].clientY
-      })
-
-      this.canvas.addEventListener("touchmove", (e) => {
-        this.x -= BigInt(Math.round(e.touches[0].clientX - this.touchX))
-        this.y -= BigInt(Math.round(e.touches[0].clientY - this.touchY))
-        this.touchX = e.touches[0].clientX
-        this.touchY = e.touches[0].clientY
-      })
+      this.canvas.addEventListener("mousedown", (e) => this.pointerDown(e.clientX, e.clientY))
+      this.canvas.addEventListener("mouseup", () => this.pointerUp())
+      this.canvas.addEventListener("mouseout", () => this.pointerUp())
+      this.canvas.addEventListener("mousemove", (e) => this.pointerMoved(e.clientX, e.clientY))
+      this.canvas.addEventListener("touchstart", (e) => this.pointerDown(e.touches[0].clientX, e.touches[0].clientY))
+      this.canvas.addEventListener("touchend", () => this.pointerUp())
+      this.canvas.addEventListener("touchmove", (e) => this.pointerMoved(e.touches[0].clientX, e.touches[0].clientY))
 
       this.canvas.addEventListener("wheel", (e) => {
-        this.x += BigInt(Math.round(e.deltaX))
-        this.y += BigInt(Math.round(e.deltaY))
-        e.preventDefault();
+        this.pointerDown(0, 0)
+        this.pointerMoved(-e.deltaX, -e.deltaY)
+        this.pointerUp()
+        e.preventDefault()
       }, { passive: false })
 
-      this.pushEvent("resize", { width: this.app.renderer.width, height: this.app.renderer.height })
       this.pushEvent("camera", { x: this.x.toString(), y: this.y.toString() })
 
       this.app.ticker.add(this.tick.bind(this))
@@ -70,25 +60,26 @@ export default {
   },
 
   tick() {
-    // handle any received actions
-    this.handleActions()
+    if (this.width !== this.app.renderer.width || this.height !== this.app.renderer.height) {
+      this.width = this.app.renderer.width
+      this.height = this.app.renderer.height
 
-    // send camera updates
-    if (Math.abs(Number(this.x - this.lastX)) >= this.tileSize * 4 || Math.abs(Number(this.y - this.lastY)) >= this.tileSize * 4) {
-      this.pushEvent("camera", { x: this.x.toString(), y: this.y.toString() })
-      this.lastX = this.x
-      this.lastY = this.y
+      // re-center the camera
+      this.app.stage.pivot.x = -Math.round(this.width / 2)
+      this.app.stage.pivot.y = -Math.round(this.height / 2)
+
+      // send new screen dimensions
+      this.pushEvent("resize", { width: this.width, height: this.height })
     }
 
-    // re-center the camera
-    this.app.stage.pivot.x = -Math.round(this.app.renderer.width / 2)
-    this.app.stage.pivot.y = -Math.round(this.app.renderer.height / 2)
-
     // update background position
-    this.bg.x = -Number(this.x % BigInt(this.tileSize)) - (Math.round(this.app.renderer.width / this.tileSize / 2) + 1) * this.tileSize
-    this.bg.y = -Number(this.y % BigInt(this.tileSize)) - (Math.round(this.app.renderer.height / this.tileSize / 2) + 1) * this.tileSize
-    this.bg.width = (this.app.renderer.width + this.tileSize * 2) / this.tileScale
-    this.bg.height = (this.app.renderer.height + this.tileSize * 2) / this.tileScale
+    this.bg.x = -Number(this.x % BigInt(this.tileSize)) - (Math.round(this.width / this.tileSize / 2) + 1) * this.tileSize
+    this.bg.y = -Number(this.y % BigInt(this.tileSize)) - (Math.round(this.height / this.tileSize / 2) + 1) * this.tileSize
+    this.bg.width = (this.width + this.tileSize * 2) / this.tileScale
+    this.bg.height = (this.height + this.tileSize * 2) / this.tileScale
+
+    // handle any received actions
+    this.handleActions()
 
     // update container positions
     for (let container of this.app.stage.children) {
@@ -101,16 +92,15 @@ export default {
 
   handleActions() {
     for (let [type, x, y, items] of this.actions) {
-      console.log([type, x, y, items])
       switch (type) {
         case "a":
-          this.addContainer(BigInt(x), BigInt(y), items)
+          this.takeContainer(BigInt(x), BigInt(y), items)
           break
         case "u":
           this.updateContainer(BigInt(x), BigInt(y), items)
           break
         case "r":
-          this.removeContainer(BigInt(x), BigInt(y))
+          this.releaseContainer(BigInt(x), BigInt(y))
           break
       }
     }
@@ -118,7 +108,7 @@ export default {
     this.actions.length = 0
   },
 
-  addContainer(partitionX, partitionY, items) {
+  takeContainer(partitionX, partitionY, items) {
     const container = this.containerPool.pop() || this.newContainer()
 
     for (let y = 0; y < this.partitionSize; y++) {
@@ -135,32 +125,32 @@ export default {
   },
 
   updateContainer(partitionX, partitionY, items) {
-    const container = this.app.stage.children.find(c =>
-      c.visible &&
-      c.partition &&
-      c.partition.x === partitionX &&
-      c.partition.y === partitionY
-    )
-
+    const container = this.findContainer(partitionX, partitionY)
     container.cacheAsBitmap = false
 
     for (let [x, y, t] of items) {
       const idx = y * this.partitionSize + x
       container.children[idx].texture = this.tileset[t]
     }
+
+    container.cacheAsBitmap = true
   },
 
-  removeContainer(partitionX, partitionY) {
-    const container = this.app.stage.children.find(c =>
+  releaseContainer(partitionX, partitionY) {
+    const container = this.findContainer(partitionX, partitionY)
+    container.visible = false
+    container.cacheAsBitmap = false
+    container.partition = null
+    this.containerPool.push(container)
+  },
+
+  findContainer(partitionX, partitionY) {
+    return this.app.stage.children.find(c =>
       c.visible &&
       c.partition &&
       c.partition.x === partitionX &&
       c.partition.y === partitionY
     )
-    container.visible = false
-    container.cacheAsBitmap = false
-    container.partition = null
-    this.containerPool.push(container)
   },
 
   newContainer() {
@@ -168,16 +158,8 @@ export default {
 
     container.buttonMode = true
     container.interactive = true
-    container.on("pointerup", (e) => {
-      if (!this.moved) {
-        const position = e.data.getLocalPosition(container)
-        const offsetX = Math.floor(position.x / this.tileSize)
-        const offsetY = Math.floor(position.y / this.tileSize)
-        const tileX = container.partition.x + BigInt(offsetX)
-        const tileY = container.partition.y + BigInt(offsetY)
-        this.pushEvent("reveal", { x: tileX.toString(), y: tileY.toString() })
-      }
-    })
+    container.on("pointerdown", this.containerPointerDown.bind(this))
+    container.on("pointertap", this.containerPointerTap.bind(this))
 
     for (let y = 0; y < this.partitionSize; y++) {
       for (let x = 0; x < this.partitionSize; x++) {
@@ -196,6 +178,58 @@ export default {
     this.app.stage.addChild(container)
     return container
   },
+
+  pointerDown(clientX, clientY) {
+    this.input.moving = true
+    this.input.clientX = clientX
+    this.input.clientY = clientY
+  },
+
+  pointerUp() {
+    this.input.moving = false
+  },
+
+  pointerMoved(clientX, clientY, force) {
+    if (this.input.moving) {
+      // Update client camera position
+      this.x -= BigInt(Math.round(clientX - this.input.clientX))
+      this.y -= BigInt(Math.round(clientY - this.input.clientY))
+      this.input.clientX = clientX
+      this.input.clientY = clientY
+
+      // Update server camera position
+      const updateDiffX = Math.abs(Number(this.x - this.input.updateX))
+      const updateDiffY = Math.abs(Number(this.y - this.input.updateY))
+
+      if (updateDiffX >= this.textureSize || updateDiffY >= this.textureSize) {
+        this.pushEvent("camera", { x: this.x.toString(), y: this.y.toString() })
+        this.input.updateX = this.x
+        this.input.updateY = this.y
+      }
+    }
+  },
+
+  containerPointerDown(e) {
+    this.input.startClientX = e.data.global.x
+    this.input.startClientY = e.data.global.y
+  },
+
+  containerPointerTap(e) {
+    const container = e.target
+    const clientX = e.data.global.x
+    const clientY = e.data.global.y
+    const diffX = Math.abs(this.input.startClientX - clientX)
+    const diffY = Math.abs(this.input.startClientY - clientY)
+
+    if (diffX < 4 && diffY < 4) {
+      const position = e.data.getLocalPosition(container)
+      const offsetX = Math.floor(position.x / this.tileSize)
+      const offsetY = Math.floor(position.y / this.tileSize)
+      const tileX = container.partition.x + BigInt(offsetX)
+      const tileY = container.partition.y + BigInt(offsetY)
+      this.pushEvent("reveal", { x: tileX.toString(), y: tileY.toString() })
+    }
+  }
 }
 
 function loadTileset(texture, tileset) {
