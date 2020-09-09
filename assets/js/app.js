300e99229f7c357f0fab2c9af4901567ef4ecad5
szlend <simon@zlender.si>
2020-09-09 20:30:04 +0200

    Disable liveSocket debugging

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,4 +31,4 @@ liveSocket.connect()
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)
-window.liveSocket = liveSocket
+// window.liveSocket = liveSocket
