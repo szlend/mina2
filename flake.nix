7098e3901a0fe996b0478a147687568ce1535e6e
Simon Žlender <pub.git@zlender.si>
2021-01-05 21:45:29 +0100

    Use nix flakes

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+  };
+
+  outputs =
+    { self, nixpkgs, flake-utils, ... }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      with nixpkgs.legacyPackages.${system}.pkgs; rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (beam.packagesWith erlangR23).elixir_1_11
+            nodejs-12_x
+          ];
+        };
+      }
+    );
+}
--- shell.nix
+++ shell.nix
@@ -1,9 +1,8 @@
 let
-  pkgs = import <nixpkgs> {};
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  flakeCompat = fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  };
 in
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    elixir
-    nodejs-12_x
-  ];
-}
+(import flakeCompat { src = ./.; }).shellNix
