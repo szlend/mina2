86e38ae10346084ce80f57c18435ac7052cc52e1
Simon Žlender <pub.git@zlender.si>
2021-01-05 16:04:01 +0100

    Re-enable cluster tests

--- test/mina_cluster_test.exs
+++ test/mina_cluster_test.exs
@@ -12,13 +12,12 @@ defmodule MinaClusterTest do
   end
 
   describe "reveal_tile/3" do
-    @tag :skip
     test "concurrently reveals expected results across nodes", %{world: world, nodes: nodes} do
       bounds = {{bot_x, bot_y}, {top_x, top_y}} = {{-20, -20}, {19, 19}}
       positions = for x <- bot_x..top_x, y <- bot_y..top_y, do: {x, y}
       chunk_length = div(length(positions), length(nodes))
 
-      # reveal all the positions at random concurrently across nodes
+      # reveal all the positions at random, concurrently across nodes
       positions
       |> Enum.shuffle()
       |> Enum.chunk_every(chunk_length, chunk_length)
